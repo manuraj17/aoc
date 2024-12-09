4c1d66ac21fb23817b9f7c342a1417be8dc01f68
Manu Raj <git@manuraj.dev>
2024-12-09 22:57:31 +0530

    Start aoc 2024!

---
+++ aoc2024/go/day01/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func fail(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func day01(name string) int {
+	f, err := os.Open(name)
+	fail(err)
+	defer f.Close()
+
+	// using scanner
+	fsc := bufio.NewScanner(f)
+	fsc.Split(bufio.ScanLines)
+
+	var first []int
+	var second []int
+
+	// var lines []string
+	for fsc.Scan() {
+		line := fsc.Text()
+		parts := strings.Split(line, " ")
+		a, err := strconv.Atoi(parts[0])
+		fail(err)
+		b, err := strconv.Atoi(parts[len(parts)-1])
+		fail(err)
+
+		first = append(first, a)
+		second = append(second, b)
+	}
+
+	sort.Ints(first[:])
+	sort.Ints(second[:])
+
+	sum := 0
+	for i := 0; i < len(first); i++ {
+		x := (second[i] - first[i])
+		if x < 0 {
+			x = int(math.Abs(float64(x)))
+		}
+		sum += x
+
+	}
+
+	return sum
+}
+
+func day02(name string) int {
+	f, err := os.Open(name)
+	fail(err)
+	defer f.Close()
+
+	// using scanner
+	fsc := bufio.NewScanner(f)
+	fsc.Split(bufio.ScanLines)
+
+	var first []int
+	var second []int
+
+	// var lines []string
+	for fsc.Scan() {
+		line := fsc.Text()
+		parts := strings.Split(line, " ")
+		a, err := strconv.Atoi(parts[0])
+		fail(err)
+		b, err := strconv.Atoi(parts[len(parts)-1])
+		fail(err)
+
+		first = append(first, a)
+		second = append(second, b)
+	}
+
+	sort.Ints(first[:])
+	sort.Ints(second[:])
+
+	sim := 0
+	curr := 0
+
+	i := 0
+	j := 0
+	for i < len(first) {
+		if first[i] == second[j] {
+			curr += 1
+			j++
+		} else {
+			j++
+
+			if j >= len(first) {
+				x := first[i] * curr
+				sim += x
+				i++
+				curr = 0
+				j = 0
+			}
+		}
+	}
+
+	return sim
+}
+
+func main() {
+	fmt.Println(day01("inp1.txt"))
+	fmt.Println(day02("inp1.txt"))
+}
---
+++ aoc2024/go/day01/main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_day01(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "day01-1 test",
+			args: args{
+				name: "test1.txt",
+			},
+			want: 11,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := day01(tt.args.name); got != tt.want {
+				t.Errorf("day01() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_day02(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "day01-1 test",
+			args: args{
+				name: "test1.txt",
+			},
+			want: 31,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := day02(tt.args.name); got != tt.want {
+				t.Errorf("day02() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
